f21f4ecc9e6906216e08f5d57527a8d55445a2ec
AnnaRychkova1 <aanytkaa@gmail.com>
2024-01-04 18:33:21 +0000

    form add detail

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,41 +6,35 @@ const input = form.querySelector("input")
 
 form.addEventListener("submit", onFormSubmit);
 form.addEventListener("input", onFormInput);
-const exportedObject = {};
 
-populateTextarea();
+let exportedObject = {
+  email: input.value,
+  message: textarea.value
+};
 
-function onFormSubmit(event) {
-  event.preventDefault();
-
-  const exportedObject = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-  if (!exportedObject.email) {
-    delete exportedObject.email
-  }
-
-  if (!exportedObject.message) {
-    delete exportedObject.message
-  }
-  
-  console.log(exportedObject);
-  
-  localStorage.removeItem(STORAGE_KEY);
-  event.currentTarget.reset();
-}
-
-function onFormInput() {
-  exportedObject.email = input.value || "";
-  exportedObject.message = textarea.value || '';
+function onFormInput(event) {
+  exportedObject[event.target.name] = event.target.value.trim()
   localStorage.setItem(STORAGE_KEY, JSON.stringify(exportedObject));
 }
 
 function populateTextarea() {
-  const savedObject = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-  if (savedObject) {
-    input.value = savedObject.email;
-    textarea.value = savedObject.message;
-
+  if (exportedObject) {
+    input.value = exportedObject.email;
+    textarea.value = exportedObject.message;
   }
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+   if (!textarea.value || !input.value) {
+     alert("Please fill in both fields")
+     return
+  }
+
+  const savedObject = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  console.log(savedObject);
+  
+  localStorage.removeItem(STORAGE_KEY);
+  event.currentTarget.reset();
 }
